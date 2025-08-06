4ef99df1de1a86d0c8195824c83300ce31607363
Sanjay-star-03 <sanjaysaravanan9406@gmail.com>
2025-08-06 17:45:09 +0530

    my first commit

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student{
+public static void main(String[]args){
+String studentname ="kishore";
+ int tamil   = 95;
+ int english = 80;
+ int maths   = 65;
+ float per = 10.88f;
+ char grade = 'A';
+System.out.println("tamil:"+tamil);
+System.out.println("English:"+english);
+System.out.println("maths:"+maths);
+System.out.println("per:"+per);
+System.out.println("maths:"+maths);
+System.out.println("grade:"+grade);
+
+}
+}
