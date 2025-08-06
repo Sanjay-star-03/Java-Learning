02de02ec2b693e5df17ae64b10ced133f6304e48
Sanjay-star-03 <sanjaysaravanan9406@gmail.com>
2025-08-06 17:54:11 +0530

    second commit

---
+++ Student info.java
@@ -0,0 +1,12 @@
+public class Student{
+public static void main(String[]args){
+
+ int tamil       =95;
+ int English     =80;
+ int maths       =65;
+
+System.out.println("tamil");
+System.out.println("English");
+System.out.println("maths");
+}
+}
