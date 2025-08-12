2fac656bd60f86659e60f6df9aed990588c88f48
Sanjay-star-03 <sanjaysaravanan9406@gmail.com>
2025-08-12 22:30:51 +0530

    Java Learning

---
+++ Collegeinformation.java
@@ -0,0 +1,18 @@
+public class Collegeinformation {
+public static void main(String[]args){
+
+ String collegename = "Veltech ranga sanku arts college";
+ int counsellingcode = 1453;
+ int studentcount = 8500 ;
+ int deptcount = 20;
+ char NAACgrade = 'A';
+
+System.out.println("collegename"+collegename);
+System.out.println("counsellingcode:"+counsellingcode);
+System.out.println("studentcount:"+studentcount);
+System.out.println("deptcount:"+deptcount);
+System.out.println("NAACgrade:"+NAACgrade);
+
+
+}
+}
---
+++ FitnessTracker.java
@@ -0,0 +1,13 @@
+public class FitnessTracker {
+public static void main(String[]args){
+
+ String username ="Sanjay";
+ int stepswalked = 10000;
+ float caloriesburned = 400.0f ;
+ 
+System.out.println("username:"+username);
+System.out.println("stepswalked:"+stepswalked);
+System.out.println("caloriesburned:"+caloriesburned);
+
+}
+}
