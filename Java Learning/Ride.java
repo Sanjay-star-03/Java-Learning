7e82ec4a2367341617824ed1426ff0ae5eefb40e
Sanjay-star-03 <sanjaysaravanan9406@gmail.com>
2025-08-13 20:42:13 +0530

    Java Learning

---
+++ Java Learning/Ride.java
@@ -0,0 +1,12 @@
+public class Ride {
+    public static void main(String[] args) {
+        int  sum1 = 100+50;
+        int sum2 = sum1+50;
+        int sum3 = sum2+sum2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+    }
+}
+
